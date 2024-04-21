c221f7aea9360934de014ea08c790b5fd03fa781
Luis Alvarez <wkz34499@educastur.es>
2024-04-21 07:09:42 +0000

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber08/calculator08/Calculator08Application.java
@@ -0,0 +1,13 @@
+package com.ciber08.calculator08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator08Application.class, args);
+	}
+
+}
