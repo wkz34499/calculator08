b0ff274765fbb6e9b0f0980999762a251a22cfd8
wkz34499 <164056154+wkz34499@users.noreply.github.com>
2024-04-23 20:08:14 +0200

    Añadido

--- src/main/java/com/ciber08/calculator08/Calculator.java
+++ src/main/java/com/ciber08/calculator08/Calculator.java
@@ -8,43 +8,13 @@ public class Calculator {
          return a + b; }
       int password = 1234;
      } 
-
- import java.sql.*;
-
-public class UnsafeSQLExample {
-    public static void main(String[] args) {
-        String userInput = args[0]; // Supongamos que esta es la entrada del usuario, por ejemplo, obtenida de un formulario web
-
-        // Conexión a la base de datos (suponiendo que existe una tabla llamada 'usuarios' con columnas 'nombre' y 'contraseña')
-        String url = "jdbc:mysql://localhost:3306/mydatabase";
-        String user = "username";
-        String password = "password";
-
-        try {
-            Connection connection = DriverManager.getConnection(url, user, password);
-
-            // Crear una declaración SQL insegura concatenando la entrada del usuario directamente en la consulta
-            String sqlQuery = "SELECT * FROM usuarios WHERE nombre = '" + userInput + "'";
-            Statement statement = connection.createStatement();
-
-            // Ejecutar la consulta
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
-
-            // Procesar los resultados
-            while (resultSet.next()) {
-                String nombreUsuario = resultSet.getString("nombre");
-                String contraseña = resultSet.getString("contraseña");
-                System.out.println("Nombre de usuario: " + nombreUsuario + ", Contraseña: " + contraseña);
-            }
-
-            // Cerrar recursos
-            resultSet.close();
-            statement.close();
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-}
+ $user = "Luis"
+ function f() {
+  i =1; 
+ for (j=0;j<arrat.length;j++)
+  too
+   }
 
 }
+
+
