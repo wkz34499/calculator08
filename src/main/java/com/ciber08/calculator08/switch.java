25dcf9e2cb54b2e59c36c3e645df35504a9cb320
wkz34499 <164056154+wkz34499@users.noreply.github.com>
2024-04-23 20:29:35 +0200

    Update switch.java mejorado

--- src/main/java/com/ciber08/calculator08/switch.java
+++ src/main/java/com/ciber08/calculator08/switch.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
+import java.util.logging.*;
 
 public class EjemploSwitch {
+    // Create a logger instance
+    private static final Logger logger = Logger.getLogger(EjemploSwitch.class.getName());
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Elije una opción:");
-        System.out.println("1. Saludar");
-        System.out.println("2. Despedir");
-        System.out.println("3. Agradecer");
+        logger.info("Elije una opción:");
+        logger.info("1. Saludar");
+        logger.info("2. Despedir");
+        logger.info("3. Agradecer");
 
         int opcion = scanner.nextInt();
 
         switch (opcion) {
             case 1:
-                System.out.println("¡Hola!");
+                logger.info("¡Hola!");
                 break;
             case 2:
-                System.out.println("¡Adiós!");
-                break;
-            default:
-                System.out.println("Opción no válida.");
+                logger.info("¡Adiós!");
                 break;
             case 3:
-                System.out.println("¡Gracias!");
+                logger.info("¡Gracias!");
+                break;
+            default:
+                logger.warning("Opción no válida.");
                 break;
         }
 
         scanner.close();
     }
 }
+
