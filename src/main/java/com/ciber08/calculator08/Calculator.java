3e177bf1dcc433cd385149c3294fc8c4d4973ad1
wkz34499 <164056154+wkz34499@users.noreply.github.com>
2024-04-23 20:15:37 +0200

    Update Calculator.java2

--- src/main/java/com/ciber08/calculator08/Calculator.java
+++ src/main/java/com/ciber08/calculator08/Calculator.java
@@ -1,20 +1,25 @@
-package com.ciber08.calculator08; 
-import org.springframework.stereotype.Service; 
- 
-@Service 
-public class Calculator { 
-     int sum(int a, int b) { 
-        if(true) {
-         return a + b; }
-      int password = 1234;
-     } 
- $user = "Luis"
- function f() {
-  i =1; 
- for (j=0;j<arrat.length;j++)
-  too
-   }
+package com.ciber08.calculator08;
 
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    
+    public int multiply(int a, int b) { return a * b; }
+    
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        return (double) a / b;
+    }
 }
 
 
